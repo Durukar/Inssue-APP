896bf4920aed2047a794c0ac90cbc24d2ebdde38
Lucas Davila <nightdavila@gmail.com>
2023-07-04 01:38:14 -0300

    Criado controller inicial para testar dependencias e esta funcional.

---
+++ backend/src/main/java/lu/inssue/backend/controller/HelloController.java
@@ -0,0 +1,16 @@
+package lu.inssue.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+
+    @GetMapping
+    public String test() {
+        return "Funcionando Perfeitamente!!!";
+    }
+
+}
