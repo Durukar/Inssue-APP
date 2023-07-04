377bbc0d3f84bbbad114976522ff5e8991f99f16
Lucas Davila <nightdavila@gmail.com>
2023-07-03 22:11:49 -0300

    Criada pasta backend para o backend do projeto e adicionado os arquivos maven

---
+++ backend/src/main/java/app/inssue/Inssue/InssueAppApplication.java
@@ -0,0 +1,13 @@
+package app.inssue.Inssue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InssueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InssueAppApplication.class, args);
+	}
+
+}
